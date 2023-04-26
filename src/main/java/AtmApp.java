78c10e3b91697b82f6d390b4e2e904d9b8edac51
Murat <scnc2626@gmail.com>
2023-04-26 12:02:22 +0300

    v2

--- src/main/java/AtmApp.java
+++ src/main/java/AtmApp.java
@@ -1,4 +1,8 @@
 public class AtmApp {
     public static void main(String[] args) {
     System.out.println("Bu bir ATM projesidir.");
+    //versiyon01
+        System.out.println("java is easy");
+    //versiyon02
+
 }}
